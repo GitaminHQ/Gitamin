58be15d8b37748fa6ec2ba1033b1681f93fd8b48
phecho <phecho@phecho.com>
2015-12-17 11:49:31 +0800

    Added moment events for Comments

--- resources/views/default/moments/partials/comment.blade.php
+++ resources/views/default/moments/partials/comment.blade.php
@@ -6,15 +6,15 @@
 <div class="moment-title">
 <i class="{{ $moment->icon }}"></i> <span class="author_name"><a href="/u/root">{{ $moment->author->username }}</a></span>
 <span class="event_label opened">
-{{ $moment->actionName }} issue
+{{ $moment->actionName }}
 </span>
-<strong><a href="/baidu/good/milestones/1">#{{ $moment->target_id }}</a></strong>
+<strong> {{ $moment->target->target_type }} <a href="{{ $moment->target->target->url }}">#{{ $moment->target->target_id }}</a></strong>
 at
-<a href="/baidu/good"><span class="namespace-name">{{ $moment->target->project->owner_path }} / </span><span class="project-name">{{ $moment->target->project->name }}</span></a>
+<a href="{{ $moment->target->project->url }}"><span class="namespace-name">{{ $moment->target->project->owner_path }} / </span><span class="project-name">{{ $moment->target->project->name }}</span></a>
 </div>
 <div class="moment-body">
 <div class="moment-comment">
-{!! $moment->formattedTarget !!} 
+{{ $moment->target->message }} 
 </div>
 </div>
 
