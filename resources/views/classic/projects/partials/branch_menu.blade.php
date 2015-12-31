205c95d3495edb0fa8d42a6868c3a468d01ec486
phecho <phecho@phecho.com>
2015-12-31 15:43:35 +0800

    Fixes breadcrumb menu

--- resources/views/classic/projects/partials/branch_menu.blade.php
+++ resources/views/classic/projects/partials/branch_menu.blade.php
@@ -1,9 +1,9 @@
 <div class="btn-group pull-left space-right">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">browsing: <strong>master</strong> <span class="caret"></span></button>
+    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">browsing: <strong>{{ $current_branch }}</strong> <span class="caret"></span></button>
     <ul class="dropdown-menu">
         <li class="dropdown-header">Branches</li>
         @foreach($branches as $branch)
-        <li><a href="{{ $project->url }}/tree/master/">{{ $branch }}</a></li>
+        <li><a href="{{ $project->url }}/tree/{{ $branch }}">{{ $branch }}</a></li>
         @endforeach
     </ul>
 </div>
--- resources/views/classic/projects/partials/menu.blade.php
+++ resources/views/classic/projects/partials/menu.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs">
-    <li @if($active_item == 'project_show') class="active" @endif><a href="/Baidu/api/tree/master/">Files</a></li>
-    <li @if($active_item == 'commits') class="active" @endif><a href="/Baidu/api/commits/master">Commits</a></li>
-    <li @if($active_item == 'stats') class="active" @endif><a href="/Baidu/api/stats/master">Stats</a></li>
-    <li @if($active_item == 'network') class="active" @endif><a href="/Baidu/api/network?branch=master">Network</a></li>
+    <li @if($active_item == 'project_show') class="active" @endif><a href="{{ $project->url }}/tree/{{ $current_branch }}">Files</a></li>
+    <li @if($active_item == 'commits') class="active" @endif><a href="{{ $project->url }}/commits/{{ $current_branch }}">Commits</a></li>
+    <li @if($active_item == 'stats') class="active" @endif><a href="{{ $project->url }}/stats/{{ $current_branch }}">Stats</a></li>
+    <li @if($active_item == 'network') class="active" @endif><a href="{{ $project->url }}/network?branch={{ $current_branch }}">Network</a></li>
 </ul>
