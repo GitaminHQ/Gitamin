ffcb8eea056160516653292b3e425bd78ddef4e3
phecho <phecho@phecho.com>
2016-01-06 11:53:13 +0800

    Clean up

--- resources/views/classic/dashboard/index.blade.php
+++ resources/views/classic/dashboard/index.blade.php
@@ -1,31 +1,30 @@
 @extends('layout.dashboard')
 
 @section('body')
-    @include('dashboard.partials.navigation')
-    <div class="container">
-        <div class="row">
-     <div class="col-xs-12 col-sm-9 main">
-     @include('dashboard.partials.errors')
-        @foreach($moments as $moment)
-        @include('moments.partials.' . $moment->template)
-        @endforeach
-     </div>
+@include('dashboard.partials.navigation')
+<div class="container">
+    <div class="row">
+         <div class="col-xs-12 col-sm-9 main">
+         @include('dashboard.partials.errors')
+            @foreach($moments as $moment)
+            @include('moments.partials.' . $moment->template)
+            @endforeach
+         </div>
 
-     <div class="col-xs-6 col-sm-3 sidebar">
-         <div class="list-group">
-            <div class="list-group-item active">Your Projects</div>
-            @foreach($projects as $project)
-            <a href="{{ $project->url }}" class="list-group-item"><i class="octicon octicon-repo"></i> {{ $project->name }}</a>
-            @endforeach
-          </div>
-          <div class="list-group">
-            <div class="list-group-item active">Starred Projects</div>
-            @foreach($projects as $project)
-            <a href="{{ $project->url }}" class="list-group-item"><i class="octicon octicon-repo"></i> {{ $project->name }}</a>
-            @endforeach
-          </div>
-     </div>
+         <div class="col-xs-6 col-sm-3 sidebar">
+             <div class="list-group">
+                <div class="list-group-item active">Your Projects</div>
+                @foreach($projects as $project)
+                <a href="{{ $project->url }}" class="list-group-item"><i class="octicon octicon-repo"></i> {{ $project->name }}</a>
+                @endforeach
+              </div>
+              <div class="list-group">
+                <div class="list-group-item active">Starred Projects</div>
+                @foreach($projects as $project)
+                <a href="{{ $project->url }}" class="list-group-item"><i class="octicon octicon-repo"></i> {{ $project->name }}</a>
+                @endforeach
+              </div>
+         </div>
+    </div>
 </div>
-   </div>
-
 @endsection
