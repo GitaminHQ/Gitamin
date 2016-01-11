2f3428938cda22feb9c938faa9165115f74ed460
phecho <phecho@phecho.com>
2016-01-11 21:46:45 +0800

    Added profile sidebar partial

--- resources/views/classic/layout/profile.blade.php
+++ resources/views/classic/layout/profile.blade.php
@@ -5,13 +5,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-xs-6 col-sm-3 sidebar">
-                <div class="list-group">
-                    @foreach($sub_menu as $key => $item)
-                    <a href="{{ $item['url'] }}" class="list-group-item @if($item['active']) active @endif">{{ $item['title'] }}</a>
-                    @endforeach
-                </div>
-            </div>
+            @include('profiles.partials.sidebar')
             @yield('content')
         </div>
         @include('dashboard.partials.footer')
