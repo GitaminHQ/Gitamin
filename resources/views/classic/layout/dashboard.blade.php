892fa266b8e0ecea521c7aef6e29c372f8c58e2b
phecho <phecho@phecho.com>
2016-01-26 10:08:36 +0800

    Cleaned up

--- resources/views/classic/layout/dashboard.blade.php
+++ resources/views/classic/layout/dashboard.blade.php
@@ -14,10 +14,6 @@
     <title>{{ $page_title or $app_name.' | Gitamin' }}</title>
     <link rel="stylesheet" href="{{ elixir('dist/css/classic.css') }}">
     <script src="{{ elixir('dist/js/gitamin.js') }}"></script>
-        
-        <!--[if lt IE 9]>
-        <script src="/dist/js/classic/html5.js"></script>
-        <![endif]-->
     </head>
 
     <body>
