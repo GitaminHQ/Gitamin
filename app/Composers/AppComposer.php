a52d8e2d9b9613c7f5a023f58a9074491fdafcf1
phecho <phecho@163.com>
2016-07-09 19:42:16 -0400

    Applied fixes from StyleCI

--- app/Composers/AppComposer.php
+++ app/Composers/AppComposer.php
@@ -19,7 +19,7 @@ class AppComposer
     {
         $app = [
             'theme'    => 'default',
-            'request' => [
+            'request'  => [
                 'basepath' => '',
             ],
         ];
